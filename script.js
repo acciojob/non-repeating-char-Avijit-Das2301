e1383626d3d8345ec2fcd653240e0dba7a64cb25
Avijit_das2001 <55279276+Avijit-Das2301@users.noreply.github.com>
2025-12-29 23:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ if (!str || str.length===0)
+	 return null;
+const freq={};
+
+//First Pass:Build frequency map
+for (let ch of str){
+	freqq[ch]=(freq[ch]||0)+1;
+}
+
+//second pass:find first char with frequency 1
+for (let ch of str) {
+	if (freq[ch]===1){
+		return ch;
+	}
+}
+
+//If no non-repeated character
+return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
